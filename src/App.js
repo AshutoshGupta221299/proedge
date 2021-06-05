84829a74c3de01133c2bd80fea2a0675b87ab169
AshutoshGupta221299 <www.ashutosh221299@gmail.com>
2021-06-05 20:04:07 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+  function submit(){
+    if(document.getElementById('edittext').value===''){
+        alert("null value");
+    }
+    else{
+      let txt=document.getElementById('edittext').value;
+      const data = new FormData();
+      data.append('query',txt);
+      fetch('http://localhost:4000/upload', {
+      method: 'POST',
+      body: data
+      }).then(response => {
+      response.json().then(body => {
+      console.log(body.file);
+      });
+      });
+    }
+  }
+ 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type='text' id='edittext' placeholder='enter comma separated roll nos.'></input>
+        <input type='submit' id='sub' onClick={submit}></input>
       </header>
     </div>
   );
